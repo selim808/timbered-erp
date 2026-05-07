a4be224aa1d77bf783e47a1f2b7c25278b189d41
selim808 <selim_808@hotmail.com>
2026-05-08 01:30:40 +0300

    Fix: add use client to ImportantLinks

--- components/owner/ImportantLinks.tsx
+++ components/owner/ImportantLinks.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 const ExternalIcon = () => (
   <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
     <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/>
